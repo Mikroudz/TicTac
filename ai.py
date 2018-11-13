5842483cf5a64dda64a1592456c6bd0347d9b477
Mikroudz <severi.olsbo@gmail.com>
2018-11-13 10:26:24 +0200

    Fixed bug that prevented ai from winning in some games

--- ai.py
+++ ai.py
@@ -4,19 +4,18 @@ from peli import Game
 
 class Ai(Game):
     def __init__(self):
-        self.maximum = False
         self.curBestMoveVal = -9999
         self.bestX = 0
         self.bestY = 0
         self.gameStatus = []
         self.debug = []
 
-    def minimax(self, tempGameStatus, iters, isMaximizing):
+    def minimax(self, tempGameStatus, iters, isMaximizing, alpha, beta):
         gameWinner = self.checkWin(tempGameStatus)
         if gameWinner == 'x':
             return 10-iters
         elif gameWinner == 'o':
-            return -10+iters
+            return iters-10
 
         if not self.isEmptyPlaces():
             return 0
@@ -27,8 +26,12 @@ class Ai(Game):
                 for j in range(3):
                     if tempGameStatus[i][j] == "":
                         tempGameStatus[i][j] = 'x'
-                        bestPlaceScore = max(bestPlaceScore, self.minimax(tempGameStatus, iters+1, not self.maximum))
+                        bestPlaceScore = max(bestPlaceScore, self.minimax(tempGameStatus, iters, not isMaximizing, alpha, beta))
                         tempGameStatus[i][j] = ""
+                        if alpha < bestPlaceScore:
+                            alpha = bestPlaceScore
+                        if alpha >= beta:
+                            break
             return bestPlaceScore
         else:
             bestPlaceScore = 9999
@@ -36,8 +39,12 @@ class Ai(Game):
                 for j in range(3):
                     if tempGameStatus[i][j] == "":
                         tempGameStatus[i][j] = 'o'
-                        bestPlaceScore = min(bestPlaceScore, self.minimax(tempGameStatus, iters+1, not self.maximum))
+                        bestPlaceScore = min(bestPlaceScore, self.minimax(tempGameStatus, iters, not isMaximizing, alpha, beta))
                         tempGameStatus[i][j] = ""
+                        if beta > bestPlaceScore:
+                            beta = bestPlaceScore
+                        if alpha >= beta:
+                            break
             return bestPlaceScore
 
     def makeBestMove(self,boardStatus):
@@ -46,13 +53,14 @@ class Ai(Game):
             for j in range(3):
                 if self.gameStatus[i][j] == "":
                     self.gameStatus[i][j] = 'x'
-                    bestMove = self.minimax(self.gameStatus, 0, self.maximum)
+                    self.maximum = False
+                    bestMove = self.minimax(self.gameStatus, 0, self.maximum, -1000, 1000)
                     self.debug.append([i, j, bestMove])
                     self.gameStatus[i][j] = ""
                     if self.curBestMoveVal < bestMove:
                         self.curBestMoveVal = bestMove
                         self.bestY = i
                         self.bestX = j
-        self.debug.append([self.bestY,self.bestX])
+        #self.debug.append([self.bestY,self.bestX])
         #import pdb; pdb.set_trace()
         return str(self.bestY) + str(self.bestX)
