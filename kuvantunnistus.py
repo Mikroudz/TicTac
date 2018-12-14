396b315c5d17a218889be1f0bffe81f1615ebf7a
Mikroudz <severi.olsbo@gmail.com>
2018-12-14 10:55:53 +0200

    Detecting x's and o's now better

--- kuvantunnistus.py
+++ kuvantunnistus.py
@@ -3,25 +3,25 @@ import numpy as np
 import imutils
 from skimage import exposure
 
-src = cv2.imread('testikuvat/current.jpg')
+src = cv2.imread('testikuvat/current4.jpg')
 
 def detectShape(c):
     shape = "empty or not found"
     peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+    approx = cv2.approxPolyDP(c, 0.03 * peri, True)
 
     if len(approx) == 3:
-        shape = "triangle"
-    elif len(approx) == 4:
-        shape = "square"
+        shape = "X"
+    elif len(approx) > 6 and cv2.contourArea(c) > 40:
+        shape = "O"
     else:
-        shape = "circle"
+        shape = "tyhja"
     return shape
 
 img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+img = exposure.rescale_intensity(img, out_range = (0, 255))
 img = cv2.fastNlMeansDenoising(img, None, 9, 13)
-denoiced = cv2.GaussianBlur(img, (3, 3), 0).astype('uint8')
-tresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+tresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,115,1)
 
 laplacian = cv2.Laplacian(tresh,cv2.CV_8U)
 
@@ -44,7 +44,7 @@ screenCnt = None
 for c in cnts:
 	# approximate the contour
     peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    approx = cv2.approxPolyDP(c, 0.1 * peri, True)
 
 	# if our approximated contour has four points, then
 	# we can assume that we have found our screen
@@ -54,6 +54,7 @@ for c in cnts:
 
 #Piirtää alueen alkuperäiseen kuvaan
 #cv2.drawContours(src, [screenCnt], -1, (0, 255, 0), 3)
+
 
 pts = screenCnt.reshape(4, 2)
 rect = np.zeros((4, 2), dtype = "float32")
@@ -88,26 +89,57 @@ print(maxWidth)
 print(maxHeight)
 
 #Korjatun ja varpatun kuvan syynäyksen aloitus
-warp = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
-warp = exposure.rescale_intensity(warp, out_range = (0, 255))
-warp_tresh = cv2.adaptiveThreshold(warp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,101,1)
+warp_gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+warp_gray = exposure.rescale_intensity(warp_gray, out_range = (0, 255))
+#warp_gray = cv2.bilateralFilter(warp_gray, 2, 100, 100)
+#warp_gray = cv2.threshold(warp_gray, 100, 255, cv2.THRESH_BINARY)[1]
+
+warp_tresh = cv2.adaptiveThreshold(warp_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,101,1)
 warp_laplacian = cv2.Laplacian(warp_tresh,cv2.CV_8U)
 
 warp_absd = cv2.convertScaleAbs(warp_laplacian)
+warp_edged = cv2.Canny(warp_gray, 30,150, apertureSize = 3)
+
+kernel = np.ones((3,3),np.uint8)
+warp_edged = cv2.dilate(warp_edged,kernel, iterations = 1)
+#kernel = np.ones((2,2),np.uint8)
+#warp_edged = cv2.erode(warp_edged,kernel,iterations = 1)
 
 x = []
 for i in range(3):
     for j in range(3):
-        x.append(warp_absd[int(i*maxHeight/3):int((i+1)*maxHeight/3),int(j*maxWidth/3):int((j+1)*maxWidth/3)].copy())
+        x.append(warp_edged[int(i*maxHeight/3):int((i+1)*maxHeight/3),int(j*maxWidth/3):int((j+1)*maxWidth/3)].copy())
 
-cnts = cv2.findContours(x[6].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+for i in range(9):
+    cnts = cv2.findContours(x[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+    shape = []
+    for c in cnts:
+        if cv2.contourArea(c) > 40:
+            shape.append(detectShape(c))
+    count_x = shape.count('X')
+    count_o = shape.count('O')
 
-for c in cnts:
-    print(detectShape(c))
+    #circles = cv2.HoughCircles(x[i].copy(),cv2.HOUGH_GRADIENT, 1.2, 10)
+    #if circles is not None:
+    #	circles = np.round(circles[0, :]).astype("int")
 
-cv2.imshow('part',x[6])
-cv2.imshow('gray',warp_tresh)
-cv2.imshow('image',src)
+    #	for (x, y, r) in circles:
+    #		cv2.circle(warp_edged, (x+i % 3 * (maxWidth // 3), y+i // 3 * (maxHeight // 3)), r, (0, 255, 0), 4)
+
+    if count_o > count_x and count_o > 1:
+        shape = 'O'
+    elif count_o < count_x and count_x > 1:
+        shape = 'X'
+    else:
+        shape = 'Tyhja'
+    tX = i % 3 * (maxWidth / 3) + (35 if shape[0] == "X" or shape[0] == "O" else 10)
+    tY = i // 3 * (maxHeight / 3) + 50
+    cv2.putText(warp, shape, (int(tX), int(tY)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+cv2.imshow('part',warp_edged)
+cv2.imshow('final',warp)
+cv2.imshow('image',tresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
